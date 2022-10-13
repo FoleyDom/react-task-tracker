e801df867d7bc8792a884c22f05818277e3e5308
Dominique Foley <dfoleydev@gmail.com>
2022-10-13 17:19:05 -0400

    Add Delete function and Toggle function to app.js. Add onDelete, onToggle props to Task.js. Add react-icon to Task.js component for deleting, use ternary operator to toggle reminder

--- src/App.js
+++ src/App.js
@@ -25,10 +25,26 @@ const App = () => {
         },
     ])
 
+    // Delete Task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+        setTasks(
+            tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)),
+        )
+    }
+
     return (
         <div className="container">
             <Header />
-            <Tasks tasks={tasks} />
+            {tasks.length > 0 ? (
+                <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+            ) : (
+                'No tasks to display'
+            )}
         </div>
     )
 }
