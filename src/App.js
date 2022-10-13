12d261e9b63517c1b8faf6cb43273ff53cca9d2b
Dominique Foley <dfoleydev@gmail.com>
2022-10-13 15:34:35 -0400

    Add Task and Tasks components to show task that have been created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'test',
+            other: 'no-show',
+            day: 'monday',
+            reminder: true,
+        },
+        {
+            text: 'Test4',
+            day: 'Wednesday',
+            reminder: false,
+            id: 3,
+        },
+        {
+            text: 'Task6',
+            day: 'Thursday',
+            reminder: true,
+            id: 4,
+        },
+    ])
+
     return (
         <div className="container">
             <Header />
+            <Tasks tasks={tasks} />
         </div>
     )
 }
