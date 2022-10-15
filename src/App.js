2b0a082309e824fbdc159308945bd30120e13116
Dominique Foley <dfoleydev@gmail.com>
2022-10-15 09:38:24 -0400

    Add changed state of showAddTask and onAdd prop to Header to toggle form with button. In Header.js add onAdd and showAdd and conditionals to button that change with state.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+    const [showAddTask, setShowAddTask] = useState(false)
+
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -28,7 +30,9 @@ const App = () => {
 
     // Add Task
     const addTask = (task) => {
-        console.log(task)
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = { id, ...task }
+        setTasks([...tasks, newTask])
     }
 
     // Delete Task
@@ -45,8 +49,8 @@ const App = () => {
 
     return (
         <div className="container">
-            <Header />
-            <AddTask onAdd={addTask} />
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ? (
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
             ) : (
